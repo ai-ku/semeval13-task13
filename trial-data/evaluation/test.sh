4a8537acbfc9fb0408b6cef5cd6f27230d0656e9
osmanbaskaya <obaskaya@ku.edu.tr>
2013-05-27 23:16:28 +0300

    updated datasets

--- trial-data/evaluation/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-for n in {1..100}
-do
-    echo $n
-    awk '{printf("%s %s",$1,$2);for(i=0;i<'$n';i++){printf(" %s_%d/1",$1,i)}printf("\n")}' keys/gold-standard/trial.gold-standard.key > out.key
-    java -jar supervised/weighted-tau.jar --no-remapping keys/gold-standard/trial.gold-standard.key out.key
-    rm out.key
-done
